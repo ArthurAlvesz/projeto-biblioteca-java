e7137fc6f9fdb70db72ec44061c96211b71c5658
Arthur <arthuralvesds4@gmail.com>
2024-05-13 17:14:42 -0300

    Adicionando itens modificados recentemente

--- src/Sistema/Funcionario.java
+++ src/Sistema/Funcionario.java
@@ -12,7 +12,24 @@ public class Funcionario extends Thread {
     }
 
     public void run() {
-      
+        while (true) {
+            synchronized (loja) {
+                if (loja.getContaDaLoja().getSaldo() >= 1400) {
+                    loja.getContaDaLoja().sacar(1400); // Sacar o salário da conta da loja
+                    salario.depositar(1400); // Receber o salário
+                    double valorInvestimento = 1400 * 0.2; // Calcular valor do investimento (20% do salário)
+                    investimentos.depositar(valorInvestimento); // Investir parte do salário
+                    System.out.println("Salário recebido e investido em contas de investimento");
+                    System.out.println("Salário atual: " + salario.getSaldo());
+                    System.out.println("Investimentos atuais: " + investimentos.getSaldo());
+                }
+            }
+            try {
+                Thread.sleep(1000); // Espera 1 segundo antes de verificar novamente
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public Conta getSalario() {
